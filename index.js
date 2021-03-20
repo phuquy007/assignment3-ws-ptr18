7adfbccfadaa7f8839106d30b0abb5b5b3463c0f
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-19 22:48:47 -0700

    Added and used inventoryRoute

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parsser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.json());
+
+const inventoryRoute = require('./routes/inventoryRoutePtr18');
+
+app.use('/inventoryPtr18', inventoryRoute);
 
 app.listen(port, () =>{
     console.log(`Web Service is listening on port ${port}`);
