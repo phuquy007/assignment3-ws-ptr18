cac326eb62b48b9a07524dff6d1da9992e1d0be9
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-20 20:51:47 -0700

    Used cors

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+const corsOptions = {
+    origin: '*'
+}
+
+app.use(cors());
+
 app.use(bodyParser.json());
 
 const inventoryRoute = require('./routes/inventoryRoutePtr18');
 const cartRoute = require('./routes/cartRoutePtr18');
 
-app.use('/inventoryPtr18', inventoryRoute);
-app.use('/cartPtr18', cartRoute);
+app.use('/inventoryPtr18',cors(corsOptions), inventoryRoute);
+app.use('/cartPtr18',cors(corsOptions), cartRoute);
 
 app.listen(port, () =>{
     console.log(`Web Service is listening on port ${port}`);
