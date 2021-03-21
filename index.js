3c66d786687af5256d48d872201780e4d6504f78
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-20 20:45:09 -0700

    Added models to save the array of inventory

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const port = 8000;
 
 app.use(bodyParser.json());
 
-let carts = [];
-
 const inventoryRoute = require('./routes/inventoryRoutePtr18');
+const cartRoute = require('./routes/cartRoutePtr18');
 
 app.use('/inventoryPtr18', inventoryRoute);
+app.use('/cartPtr18', cartRoute);
 
 app.listen(port, () =>{
     console.log(`Web Service is listening on port ${port}`);
