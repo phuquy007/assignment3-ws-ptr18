119d0215d38777587880eccb9f68aa0b55a93d11
Ryan Tran <tranp6@student.douglascollege.ca>
2021-03-20 19:05:38 -0700

    Added cartRoute

---
+++ routes/cartRoutePtr18.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var cartController = require('../controllers/cartControllerPtr18');
+
+router.get('/', cartController.getCartPtr18);
+router.post('/', cartController.addItemPtr18);
+router.delete('/', cartController.removeItemPtr18);
+router.post('/checkout', cartController.checkoutPtr18);
+
+module.exports = router;
